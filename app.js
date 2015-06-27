360baf4c3f38da7e54344754a7a411d180b3f365
Irfan Smajevic <smajevic.irfan@gmail.com>
2015-06-28 00:03:49 +0200

    Added layout for header and footer

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+/*
+app.use(require('node-sass-middleware')({
+	src: path.join(__dirname, 'src'),
+	dest: path.join(__dirname, 'public'),
+	debug: true,
+	outputStyle: 'compressed',
+	prefix: '/styles'
+}));
+*/
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Setting of routes
@@ -23,4 +32,4 @@ var server = http.createServer(app);
 
 server.listen(app.get('port'), function() {
 	console.log('Express server listening on port ' + app.get('port'));
-})
+}, '0.0.0.0');
--- gulpfile.js
+++ gulpfile.js
@@ -15,39 +15,39 @@ var gulp = require('gulp'),
  
 // Styles
 gulp.task('styles', function() {
-  return sass('public/css/*.scss', { style: 'expanded' })
+  return sass('src/css/main.scss', { style: 'expanded' })
     .pipe(autoprefixer('last 2 version'))
-    .pipe(gulp.dest('rendered/css'))
+    .pipe(gulp.dest('public/css'))
     .pipe(rename({ suffix: '.min' }))
     .pipe(minifycss())
-    .pipe(gulp.dest('rendered/css'))
+    .pipe(gulp.dest('public/css'))
     .pipe(notify({ message: 'Styles task complete' }));
 });
  
 // Scripts
 gulp.task('scripts', function() {
-  return gulp.src('public/js/**/*.js')
+  return gulp.src('src/js/**/*.js')
     .pipe(jshint(/*'.jshintrc'*/))
     .pipe(jshint.reporter('default'))
     .pipe(concat('main.js'))
-    .pipe(gulp.dest('rendered/js'))
+    .pipe(gulp.dest('public/js'))
     .pipe(rename({ suffix: '.min' }))
     .pipe(uglify())
-    .pipe(gulp.dest('rendered/js'))
+    .pipe(gulp.dest('public/js'))
     .pipe(notify({ message: 'Scripts task complete' }));
 });
  
 // Images
 gulp.task('images', function() {
-  return gulp.src('public/img/**/*')
+  return gulp.src('src/img/**/*')
     .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-    .pipe(gulp.dest('rendered/img'))
+    .pipe(gulp.dest('public/img'))
     .pipe(notify({ message: 'Images task complete' }));
 });
  
 // Clean
 gulp.task('clean', function(cb) {
-    del(['rendered/css', 'rendered/js', 'rendered/img'], cb)
+    del(['public/css', 'public/js', 'public/img'], cb)
 });
  
 // Default task
@@ -59,18 +59,18 @@ gulp.task('default', ['clean'], function() {
 gulp.task('watch', function() {
  
   // Watch .scss files
-  gulp.watch('public/css/**/*.scss', ['styles']);
+  gulp.watch('src/css/**/*.scss', ['styles']);
  
   // Watch .js files
-  gulp.watch('public/js/**/*.js', ['scripts']);
+  gulp.watch('src/js/**/*.js', ['scripts']);
  
   // Watch image files
-  gulp.watch('public/img/**/*', ['images']);
+  gulp.watch('src/img/**/*', ['images']);
  
   // Create LiveReload server
   livereload.listen();
  
-  // Watch any files in rendered/, reload on change
-  gulp.watch(['rendered/**']).on('change', livereload.changed);
+  // Watch any files in public/, reload on change
+  gulp.watch(['public/**']).on('change', livereload.changed);
  
 });
